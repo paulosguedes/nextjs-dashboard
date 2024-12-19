6188bc9cb40185d7ba0e0178c2c9828129adff10
Paulo Sérgio Guedes <47933174+paulosguedes@users.noreply.github.com>
2024-12-19 14:54:47 -0300

    ESLint error fixes 3

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -110,7 +110,6 @@ export async function deleteInvoice(id: string) {
   try {
     await sql`DELETE from invoices WHERE id = ${id}`;
     revalidatePath("/dashboard/invoices");
-    return { message: "Deleted Invoice." };
   } catch (error) {
     return { message: "Database Error: Failed to Delete Invoice." + error };
   }
