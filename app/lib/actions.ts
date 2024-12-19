388d0b1a2a7037bd61ffa38912b924bb0b6dba1e
Paulo Sérgio Guedes <47933174+paulosguedes@users.noreply.github.com>
2024-12-19 12:35:18 -0300

    ESLint errors fixes

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -59,7 +59,7 @@ export async function createInvoice(prevState: State, formData: FormData) {
       INSERT INTO invoices (customer_id, amount, status, date)
       VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
     `;
-  } catch (e) {
+  } catch () {
     // If a database error occurs, return a more specific error.
     return {
       message: "Database Error: Failed to Create Invoice.",
@@ -97,7 +97,7 @@ export async function updateInvoice(
     SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
     WHERE id = ${id}
   `;
-  } catch (e) {
+  } catch () {
     return {
       message: "Database Error: Failed to Create Invoice.",
     };
@@ -111,7 +111,7 @@ export async function deleteInvoice(id: string) {
     await sql`DELETE from invoices WHERE id = ${id}`;
     revalidatePath("/dashboard/invoices");
     return { message: "Deleted Invoice." };
-  } catch (e) {
+  } catch () {
     return { message: "Database Error: Failed to Delete Invoice." };
   }
 }
